aad2b9ad36edea7c4c01ec18dfaf828beb280ef4
Mariah <alinemariah@hotmail.com>
2025-02-02 19:11:06 +0100

    bonus done

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,18 @@ import ExponentThree from "./components/ExponentThree";
 import ExponentFour from "./components/ExponentFour";
 import ExponentFive from "./components/ExponentFive";
 import ExponentSix from "./components/ExponentSix";
-
+import { useState } from "react";
 
 function App () {
+  const [count, setCount] = useState(0);
+
+
+
   return (
     <div className="App">
       <h2><em>Counter</em></h2>
   
-      <Counter/>
+      <Counter count={count} setCount={setCount}/>
 
       <br />
       <h2><em>Exponents</em></h2>
@@ -29,3 +33,4 @@ function App () {
 }
 
 export default App;
+
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const Counter = ({count, increment, decrement}) => {
+const Counter = () => {
   const [count, setCount] = useState(0);
 
   const decrement = () => setCount((prevCount) => prevCount - 1);
